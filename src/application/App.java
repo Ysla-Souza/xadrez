af0a7d9f9e2eec9f8d6441367496e72ea999908d
Yxla Souza <yslasouzagnr@gmail.com>
2023-11-24 12:29:29 -0300

    create class board and piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class App {
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        //Position pos = new Position(3, 5);
+    
+        Board board = new Board(8, 8);
     }
 }
