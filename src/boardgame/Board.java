269d164f0dc4c1d54af7660f5cce52c67e328df2
Yxla Souza <yslasouzagnr@gmail.com>
2023-11-24 17:45:46 -0300

    boardException

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece [][] pieces;
    
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1){
+            throw new BoardException("Error");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece [rows] [columns];
@@ -28,7 +31,11 @@ public class Board {
         this.columns = columns;
     }
     
+
     public Piece piece(int row, int column){
+        if (!positionExists(row, column)){
+            throw new BoardException("Position not on the board");
+        }
         return pieces [row] [column];
 
     }
@@ -36,4 +43,28 @@ public class Board {
         return pieces [position.getRow()] [position.getColumn()];
      }
 
+     public void placePiece(Piece piece, Position position){
+        if (thereIsAPiece(position)){
+            throw new BoardException("There is already a piece on position " + position);
+        }
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+
+     } 
+     
+     private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+     }
+     
+     public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+
+     }
+
+     public boolean thereIsAPiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Position on the board");
+        }
+        return piece(position) != null;
+     }
 }
