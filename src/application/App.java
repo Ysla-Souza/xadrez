ae705eb65af3b856f8df238cea6505f9f0a67e65
Yxla Souza <yslasouzagnr@gmail.com>
2023-11-24 12:53:20 -0300

    create class, colo and ui

--- src/application/App.java
+++ src/application/App.java
@@ -1,12 +1,14 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) {
         
         //Position pos = new Position(3, 5);
-    
-        Board board = new Board(8, 8);
+        //Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
