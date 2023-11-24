849e192a2f97db0d497837feeb55cb0cdf2e5be3
Yxla Souza <yslasouzagnr@gmail.com>
2023-11-24 13:17:01 -0300

    alteração no print da classe UI

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,24 +4,23 @@ import chess.ChessPiece;
 
 public class UI {
     
-    public static void printBoard(ChessPiece[][] pieces){
-        for (int i = 0; i<pieces.length; i++){
-            System.out.println((8 - i) + " ");
-            for (int j = 0; j<pieces.length; j++){
-                printPiece(pieces[i][j]);
-            }
-            System.out.println();
+   public static void printBoard(ChessPiece[][] pieces){
+    for (int i = 0; i < pieces.length; i++){
+        System.out.print((8 - i) + " "); // Use print instead of println
+        for (int j = 0; j < pieces[i].length; j++){
+            printPiece(pieces[i][j]);
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println();
     }
+    System.out.println("  a b c d e f g h");
+}
 
-    private static void printPiece(ChessPiece piece){
-        if (piece == null) {
-            System.out.println("-");
-        }
-        else {
-            System.out.println(piece);
-        }
-        System.out.println(" ");
+private static void printPiece(ChessPiece piece){
+    if (piece == null) {
+        System.out.print("- "); // Use print instead of println
     }
+    else {
+        System.out.print(piece + " "); // Use print instead of println
+    }
+}
 }
